f509161d45a828478f26cc579ce4831d3a778cec
PaladinKnightMaster <paladinknightmaster@gmail.com>
2023-06-23 16:15:12 -0400

    complete lesson-6

--- first-app-lesson-00/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-00/src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -13,5 +14,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
