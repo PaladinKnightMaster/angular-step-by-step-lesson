107e8e430aee94ff25ce12339beb8c2d3d9d9582
PaladinKnightMaster <paladinknightmaster@gmail.com>
2023-06-23 12:52:39 -0400

    complete lesson-03

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city" >
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="result">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
