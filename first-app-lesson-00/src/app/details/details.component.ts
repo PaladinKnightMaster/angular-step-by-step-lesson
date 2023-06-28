0b703edd301df8122a90019d4512a98ca309095c
PaladinKnightMaster <paladinknightmaster@gmail.com>
2023-06-28 15:11:03 -0400

    complete final lesson

--- first-app-lesson-00/src/app/details/details.component.ts
+++ first-app-lesson-00/src/app/details/details.component.ts
@@ -55,8 +55,10 @@ export class DetailsComponent {
   });
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+    this.housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(this.housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   submitApplication() {
